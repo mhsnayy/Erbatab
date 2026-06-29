fc4ca7da666b16aa262539e1c98f0a9541f6ad29
mhsnayy <muhsinay06@gmail.com>
2026-06-29 16:52:01 +0300

    identity domain completed

---
+++ src/modules/identity/core/Identity.Domain/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity.Domain.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ src/modules/identity/core/Identity.Domain/User.cs
@@ -0,0 +1,12 @@
+﻿using Identity.Domain.Common;
+
+namespace Identity.Domain
+{
+    public class User : BaseEntity
+    {
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string HashedPassword { get; set; }
+        public required string PhoneNumber { get; set; }
+    }
+}
